17302fd60452645184434c8de8f45a7e67afec81
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2021-10-11 16:57:01 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,1 +1,13 @@
 
+import os
+import time
+import math
+import json
+import string
+import random
+import traceback
+import asyncio
+import datetime
+import aiofiles
+from random import choice
+from telegraph import upload_file
