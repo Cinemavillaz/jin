883c3fa3926b2ef16772054b9e64e0915be6e3c7
MuFaz-TG <88883729+BXBotz@users.noreply.github.com>
2021-10-11 11:23:52 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,3 +1,5 @@
 from info import ADMINS
 
 @Client.on_message(filters.text & filters.private & filters.incoming & filters.user(ADMINS)
+async def start(client, message):
+       insert(int(message.chat.id))
