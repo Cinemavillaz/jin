0e6825e10de094fe9b9b6510309ad77ae4604b14
MuFaz-TG <88883729+BXBotz@users.noreply.github.com>
2021-10-11 11:26:49 +0530

    Delete broadcast.py

--- plugins/broadcast.py
+++
@@ -1,7 +0,0 @@
-from info import ADMINS
-import os
-
-@Client.on_message(filters.text & filters.private & filters.incoming & filters.user(ADMINS)
-async def start(client, message):
-       insert(int(message.chat.id))
-    )
