66c85b542245875b42ee9d90351e8fbd0b6ed419
MuFaz-TG <88883729+BXBotz@users.noreply.github.com>
2021-10-11 19:39:52 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -13,9 +13,50 @@ from random import choice
 from telegraph import upload_file
 
 @Client.on_message(filters.command("telegraph"))
-async def start(bot, message):
+async def telegraph_upload(bot, message):
     reply = message.reply_to_message
 
     if not reply or not reply.text:
         return await message.reply("Reply to a text message")
+        return
+    medianame = "./DOWNLOADS/" + "FayasNoushad/FnTelegraphBot"
+    text = await update.reply_text(
+        text="<code>Downloading to My Server ...</code>",
+        disable_web_page_preview=True
+    )
+    await bot.download_media(
+        message=update,
+        file_name=medianame
+    )
+    await text.edit_text(
+        text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>",
+        disable_web_page_preview=True
+    )
+    try:
+        response = upload_file(medianame)
+    except Exception as error:
+        print(error)
+        await text.edit_text(
+            text=f"Error :- {error}",
+            disable_web_page_preview=True
+        )
+        return
+    try:
+        os.remove(medianame)
+    except Exception as error:
+        print(error)
+        return
+    await text.edit_text(
+        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @FayasNoushad",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
+                    InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+                ],
+                [InlineKeyboardButton(text="⚙ Join Updates Channel ⚙", url="https://telegram.me/FayasNoushad")]
+            ]
+        )
+    )
 
