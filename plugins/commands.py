5fa0b241580c9010d25b6594fdb37433a2400f85
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2021-10-12 08:57:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,5 +1,7 @@
 import os
 import logging
+from config import Config
+from .fonts import Fonts
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
