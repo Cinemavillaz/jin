a65e189c3ef1e75af7060cb5202cf0ffe5d89b61
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2021-10-22 10:24:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -152,14 +152,14 @@ async def group(client, message):
                     ]
                 )
         else:
-            Send_message = await client.send_photo(
+            send_text = await client.send_photo(
                  chat_id = message.chat.id,
                  photo="https://telegra.ph/file/eabeaf432b58f129ea2b4.jpg",
                  caption=f"<b>Hɪ {message.from_user.mention} 👋,\nCᴏᴜʟᴅɴ'ᴛ Fɪɴᴅ Tʜᴇ Mᴏᴠɪᴇ</b> :- ' {search} ' <b>Tʜᴀᴛ Yᴏᴜ Hᴀᴠᴇ Rᴇǫᴜᴇsᴛᴇᴅ ‼️...\n\nCʜᴇᴄᴋ Wʜᴇᴛʜᴇʀ Rᴇǫᴜᴇsᴛᴇᴅ Qᴜᴇʀʏ</b> ' {search}  ' <b>Is Cᴏʀʀᴇᴄᴛ Oʀ Nᴏᴛ Iɴ Gᴏᴏɢʟᴇ ❕...\n\nIғ Iᴛ's Cᴏʀʀᴇᴄᴛ Tʜᴇɴ Tᴀɢ Aᴅᴍɪɴ (@admins) Tᴏ ᴜᴘʟᴏᴀᴅ Tʜɪs</b> ' {search} ' <b>Mᴏᴠɪᴇ ❕</b>",
                  reply_to_message_id=message.message_id       
         )
         await asyncio.sleep(0)
-        await Send_message.delete()
+        await send_text.delete()
         if not btn:
             return
         if len(btn) > 10: 
