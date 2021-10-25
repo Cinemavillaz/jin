3cfe04463baf42c035871b8708f9f64f6c191186
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-10-25 20:40:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -159,7 +159,10 @@ async def group(client, message):
                 file_size = get_size(file.file_size)
                 file_link = f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}"
                 if filters:
-                buttons.insert( [InlineKeyboardButton(text="⭕️ Join Our Main Channel ⭕️",url="https://t.me/cv_updatez")] )
+                btn.insert(
+                    [
+                      InlineKeyboardButton(text="⭕️ Join Our Main Channel ⭕️", url="https://t.me/cv_updatez")]
+                )
                 btn.append(
                     [
                       InlineKeyboardButton(text=f"{file_name}", url=f"{file_link}"),
