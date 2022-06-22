cff96a23d9de38823efe20223463e90686b55653
Joel Bx <87010768+Cinemavillaz@users.noreply.github.com>
2022-06-22 21:58:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 START_MSG = """
-<i>𝖧ey,
+<i>𝖧ey {cmd.from_user.mention},
+
 𝖨 𝖺𝗆 𝖺𝗇 𝖠𝖽𝗏𝖺𝗇𝖼𝖾𝖽 𝖠𝗎𝗍𝗈-𝖿𝗂𝗅𝗍𝖾𝗋 𝖻𝗈𝗍, 𝖠𝖽𝖽 𝗆𝖾 𝗍𝗈 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉 , 𝗆𝖺𝗄𝖾 𝗆𝖾 𝖺𝖽𝗆𝗂𝗇 𝖺𝗇𝖽 𝗌𝖾𝖾 𝗆𝗒 𝗉𝗈𝗐𝖾𝗋...😝🔥</i>
 """
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
